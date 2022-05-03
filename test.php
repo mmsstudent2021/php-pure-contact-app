f68457529480bae1eb2d1bcdfd36a1a88013bacf
MMSIT <m@MMSs-iMac.local>
2022-05-04 01:14:55 +0630

    Revert "todel"
    This reverts commit 4c5869f675637b4234e1e570f6819ad871f10a50.

--- test.php
+++ test.php
@@ -7,7 +7,4 @@ require_once "functions.php";
 //echo contactCreate();
 //print_r(contactDelete(5));
 
-//echo preg_match("/^[a-zA-Z ]*$/",'Hein Htet Zan');
-
-$id = 56;
-print_r(contact($id));
+echo preg_match("/^[a-zA-Z ]*$/",'Hein Htet Zan');
